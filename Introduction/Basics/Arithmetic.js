72c183970f896e448465183c00e4cf36258b8519
Arvind Kharwal <karvind08@gmail.com>
2026-08-03 21:41:27 +0530

    3 August 2026

---
+++ Introduction/Basics/Arithmetic.js
@@ -0,0 +1,29 @@
+// Addition 
+let x = 12;
+let y = "Hello";
+let z = x + y;
+console.log(z); // Output: 12Hello
+
+// Example 2
+let a = 5.6;
+let b = 6.8;
+let s = a+b;
+console.log(s); // Output: 12.4
+
+console.log(Infinity - Infinity); // Output: NaN
+
+// Example 3
+
+var num1 = 22;
+var num2 = 28;
+var num3 = 10.2;
+var num4 = 20.6;
+console.log("22 + 28 = " + num1 + num2);   // 22 + 28 = 2228
+console.log("22 + 28 = " + (num1 + num2)); // 22 + 28 = 50
+console.log("10.2 + 20.6 = " + (num3 + num4)); // 10.2 + 20.6 = 30.8
+
+
+// Modulus
+var num5 = 3;
+var num6 = 5;
+console.log("3 % 5 = " + (num5 % num6)); // 10 % 3 = 1
