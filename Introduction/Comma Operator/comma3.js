355b81aa19e42f04f72fa74d96989e89a9905cf5
Arvind Kharwal <karvind08@gmail.com>
2026-08-03 22:23:56 +0530

    3 August 2026

--- Introduction/Comma Operator/comma3.js
+++ Introduction/Comma Operator/comma3.js
@@ -1,4 +1,7 @@
-let list = [1,2,3,4,5];
-for(i=0,j=4;i<list.length;i++,j--){
-    console.log(list[i], list[j]);
+var list = [1,2,3,4,5];
+for(let i=0,j=list.length-1;i<j;i++,j--){
+    let t =list[i];
+    list[i] = list[j];
+    list[j] = t;
 }
+ console.log(list);
