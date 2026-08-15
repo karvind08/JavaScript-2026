4058d3d26611aac13b57708da0d33581fc3f763b
Arvind Kharwal <karvind08@gmail.com>
2026-08-15 13:23:37 +0530

    15 August 2026

--- Object/object1.js
+++ Object/object1.js
@@ -4,8 +4,10 @@ let person = {
     'org type':'Edu'
 }
 person.last = "Singh";
+person.age = 44;
 console.log(person);
 console.log(person.first);
 console.log(person['last']);
+console.log("Age: "+person.age);
 console.log(person['org type']);
 console.log(person.phone);
