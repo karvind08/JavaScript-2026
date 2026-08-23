f056703baaa9753489ae41aba18b4c0c3e741c0f
Arvind Kharwal <karvind08@gmail.com>
2026-08-23 16:25:53 +0530

    23 August 2026

---
+++ String/immutable1.js
@@ -0,0 +1,5 @@
+let str = "Arvind";
+console.log(str);
+let str1 = "S"+str.slice(1);
+console.log(str1);
+console.log(str);
---
+++ String/slice.js
@@ -0,0 +1,4 @@
+const fruits = ["Apple",'banana','Grapse'];
+const copy = fruits.slice();
+console.log(fruits);
+console.log(copy);
