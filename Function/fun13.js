b5f1eb28327964e435954b4ea33650170598e70b
Arvind Kharwal <karvind08@gmail.com>
2026-08-28 21:08:36 +0530

    28 August 2026

--- Function/fun13.js
+++ Function/fun13.js
@@ -5,5 +5,8 @@ function addt(n1,n2){
 // addt();
 // addt(3,4);
 // addt(3,"5");
-addt(2,"a");
-addt(2,null);
+// addt(2,"a");
+// addt(2,null);
+
+const res = addt(10,34);
+console.log(res);
---
+++ Function/fun14.js
@@ -0,0 +1,6 @@
+function addt(n1,n2){
+    return n1+n2;
+}
+
+const res = addt(10,34);
+console.log(res);
