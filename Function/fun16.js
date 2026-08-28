25d11939419672e2dc56215be76076365c7777d2
Arvind Kharwal <karvind08@gmail.com>
2026-08-28 21:33:16 +0530

    28 August 2026

--- Function/fun16.js
+++ Function/fun16.js
@@ -1,11 +1,10 @@
 function loggedinuser(username){
     if(username===undefined){
-        console.log('Please enter correct values');
+        return 'Please enter correct values';
     }
-    else{
         return `${username} just signin`;
-    }
     
 }
 
+console.log(loggedinuser("Arvind"));
 console.log(loggedinuser());
