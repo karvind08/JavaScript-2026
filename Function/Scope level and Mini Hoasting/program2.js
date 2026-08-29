6b062a5b00e7fd0930243f4de56d15bab353678f
Arvind Kharwal <karvind08@gmail.com>
2026-08-29 12:57:30 +0530

    29 August 2026

---
+++ Function/Scope level and Mini Hoasting/program2.js
@@ -0,0 +1,11 @@
+function outer(){
+    let username = "Arvind";
+    function inner(){
+        let platform = "windows";
+        console.log(username);
+        console.log(platform);
+    }
+    // console.log(platform);
+    // inner();
+}
+outer();
