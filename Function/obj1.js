f2b688baa6d9ccedc3cc0136b8b7dc0be9a91cf5
Arvind Kharwal <karvind08@gmail.com>
2026-08-29 11:14:07 +0530

    29 August 2026

---
+++ Function/obj1.js
@@ -0,0 +1,11 @@
+const User = {
+    username: "Arvind",
+    platform: "Google"
+};
+
+function handleObject(Obj){
+    console.log(Obj.username);
+    console.log(Obj.platform);
+}
+
+handleObject(User);
