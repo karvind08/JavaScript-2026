db3f2807a7345b0809bc3f0e62154401f9656b03
Arvind Kharwal <karvind08@gmail.com>
2026-08-29 11:26:33 +0530

    29 August 2026

---
+++ Function/array1.js
@@ -0,0 +1,6 @@
+function getssecond(myarray){
+    return myarray[1];
+}
+
+arr = [10,30,45,79];
+console.log(getssecond(arr));
---
+++ Function/array2.js
@@ -0,0 +1,4 @@
+function getssecond(myarray){
+    return myarray[1];
+}
+console.log(getssecond([10,30,56,79]));
