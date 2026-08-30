14056b87785c92370c6e0401c532e24f0ebdcfdc
Arvind Kharwal <karvind08@gmail.com>
2026-08-30 14:53:00 +0530

    30 August 2026

--- Function/Test/test3.js
+++ Function/Test/test3.js
@@ -2,3 +2,5 @@ function display(a,b){
     return a+b;
 }
 console.log(display(10,20));
+let r = display(30,40);
+console.log(r);
---
+++ Function/Test/test4.js
@@ -0,0 +1,4 @@
+function display(a,b){
+    return a+b;
+}
+console.log(display(2,"ab"));
