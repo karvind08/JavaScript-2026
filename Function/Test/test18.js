b368d2cbee6588210078781f6adea2ee727a0991
Arvind Kharwal <karvind08@gmail.com>
2026-08-30 20:34:43 +0530

    30 August 2026

--- Function/Test/test18.js
+++ Function/Test/test18.js
@@ -1,5 +1,8 @@
 function fun1(){
-    return (new Date());
+    // return (new Date()).toLocaleDateString("en-US");
+    // return (new Date()).toLocaleDateString("en-IN");
+    // return (new Date());
+    return (new Date()).toLocaleDateString();
 }
 function date(x = fun1()){
     console.log(x);
