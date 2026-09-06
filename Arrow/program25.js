fe423884d824fbc78d46a7c36c6bfc5ee57d9faa
Arvind Kharwal <karvind08@gmail.com>
2026-09-06 14:22:00 +0530

    6 September 2026

---
+++ Arrow/program25.js
@@ -0,0 +1,11 @@
+function Person(name){
+    this.name = name;
+}
+Person.getName = function(){
+    return this.name;
+}
+
+let P1 = new Person("Arvind");
+console.log(P1.name);
+let P2 = new Person("Yuvaan");
+console.log(P2.name);
